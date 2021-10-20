18c0db7c04873d19114ce3349a29deba4ebb4e12
Lisa Han <ellesis@gmail.com>
2021-10-20 05:56:16 -0700

    change class to className

--- src/components/card/RiskValueAwesome.js
+++ src/components/card/RiskValueAwesome.js
@@ -8,51 +8,51 @@ export default function RiskValueAwesome() {
   return (
     <>
       {/* arrow color and risk value */}
-      <div class="p-3 border border-2 m-2">
-        <div class="fa-lg text-center fw-bold lh-base" style={{ color: '#006b00' }}>
-          <i class="fas fa-arrow-alt-circle-up"></i>
+      <div className="p-3 border border-2 m-2">
+        <div className="fa-lg text-center fw-bold lh-base" style={{ color: '#006b00' }}>
+          <i className="fas fa-arrow-alt-circle-up"></i>
           &nbsp; Great Deal <br />
           $10 Below <br />
         </div>
-        <div class="text-center fw-bold">FalconUp's Instant Market Value of $130</div>
+        <div className="text-center fw-bold">FalconUp's Instant Market Value of $130</div>
       </div>
 
-      <div class="p-3 border border-2 m-2">
-        <div class="fa-lg text-center fw-bold lh-base" style={{ color: '#ff8400' }}>
-          <i class="fas fa-arrow-alt-circle-up" style={{ transform: 'rotate(135deg)' }}></i>
+      <div className="p-3 border border-2 m-2">
+        <div className="fa-lg text-center fw-bold lh-base" style={{ color: '#ff8400' }}>
+          <i className="fas fa-arrow-alt-circle-up" style={{ transform: 'rotate(135deg)' }}></i>
           &nbsp; High Price <br />
           $10 Above <br />
         </div>
-        <div class="text-center fw-bold">FalconUp's Instant Market Value of $130</div>
+        <div className="text-center fw-bold">FalconUp's Instant Market Value of $130</div>
       </div>
 
-      <div class="p-3">
-        <div class="fa-lg" style={{ color: '#009b00' }}>
-          <i class="fas fa-arrow-alt-circle-up" style={{ transform: 'rotate(45deg)' }}></i>
+      <div className="p-3">
+        <div className="fa-lg" style={{ color: '#009b00' }}>
+          <i className="fas fa-arrow-alt-circle-up" style={{ transform: 'rotate(45deg)' }}></i>
           &nbsp; Good Deal
         </div>
-        <div class="fa-lg" style={{ color: '#00bd00' }}>
-          <i class="fas fa-arrow-alt-circle-up fa-rotate-90"></i>
+        <div className="fa-lg" style={{ color: '#00bd00' }}>
+          <i className="fas fa-arrow-alt-circle-up fa-rotate-90"></i>
           &nbsp; Fair Deal
         </div>
-        <div class="fa-lg" style={{ color: '#ff8400' }}>
-          <i class="fas fa-arrow-alt-circle-up" style={{ transform: 'rotate(135deg)' }}></i>
+        <div className="fa-lg" style={{ color: '#ff8400' }}>
+          <i className="fas fa-arrow-alt-circle-up" style={{ transform: 'rotate(135deg)' }}></i>
           &nbsp; High Price
         </div>
-        <div class="fa-lg" style={{ color: '#f60000' }}>
-          <i class="fas fa-arrow-alt-circle-up fa-rotate-180"></i>
+        <div className="fa-lg" style={{ color: '#f60000' }}>
+          <i className="fas fa-arrow-alt-circle-up fa-rotate-180"></i>
           &nbsp; Overpriced
         </div>
-        <div class="fa-lg">
-          <i class="fas fa-times-circle" style={{ color: '#707070' }}></i>
+        <div className="fa-lg">
+          <i className="fas fa-times-circle" style={{ color: '#707070' }}></i>
           &nbsp; No Price Analysis
         </div>
-        <div class="fa-lg">
-          <i class="fas fa-exclamation-circle" style={{ color: '#707070' }}></i>
+        <div className="fa-lg">
+          <i className="fas fa-exclamation-circle" style={{ color: '#707070' }}></i>
           &nbsp; No Price Analysis
         </div>
-        <div class="fa-lg">
-          <i class="fas fa-question-circle" style={{ color: '#707070' }}></i>
+        <div className="fa-lg">
+          <i className="fas fa-question-circle" style={{ color: '#707070' }}></i>
           &nbsp; Uncertain
         </div>
         <div> Price: $100 </div>
@@ -60,26 +60,26 @@ export default function RiskValueAwesome() {
         Market Value of $130
       </div>
 
-      <div class="p-3">
+      <div className="p-3">
         {/* arrow color and risk value */}
-        <div class="fa-lg align-middle" style={{ color: '#006b00' }}>
-          <i class="fas fa-arrow-alt-circle-up"></i>
+        <div className="fa-lg align-middle" style={{ color: '#006b00' }}>
+          <i className="fas fa-arrow-alt-circle-up"></i>
           &nbsp; Great Deal
         </div>
-        <div class="fa-lg">
-          <i class="fas fa-arrow-alt-circle-up" style={{ transform: 'rotate(45deg)', color: '#009b00' }}></i>
+        <div className="fa-lg">
+          <i className="fas fa-arrow-alt-circle-up" style={{ transform: 'rotate(45deg)', color: '#009b00' }}></i>
           &nbsp; Good Deal
         </div>
-        <div class="fa-lg">
-          <i class="fas fa-arrow-alt-circle-up fa-rotate-90" style={{ color: '#00bd00' }}></i>
+        <div className="fa-lg">
+          <i className="fas fa-arrow-alt-circle-up fa-rotate-90" style={{ color: '#00bd00' }}></i>
           &nbsp; Fair Deal
         </div>
-        <div class="fa-lg">
-          <i class="fas fa-arrow-alt-circle-up" style={{ transform: 'rotate(135deg)', color: '#ff8400' }}></i>
+        <div className="fa-lg">
+          <i className="fas fa-arrow-alt-circle-up" style={{ transform: 'rotate(135deg)', color: '#ff8400' }}></i>
           &nbsp; High Price
         </div>
-        <div class="fa-lg">
-          <i class="fas fa-arrow-alt-circle-up fa-rotate-180" style={{ color: '#f60000' }}></i>
+        <div className="fa-lg">
+          <i className="fas fa-arrow-alt-circle-up fa-rotate-180" style={{ color: '#f60000' }}></i>
           &nbsp; Overpriced
         </div>
         <div> Price: $100 </div>
